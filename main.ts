0596e04c8096de67413d4388a38c9e2e26335575
KeetoDigital <134749496+KeetoDigital@users.noreply.github.com>
2023-05-26 19:34:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showString("My name is: ");
+basic.showString("Marcos!");
+basic.showString("My age is: ")
+basic.showNumber(36);
+basic.showString("Favorite color: ")
+basic.showString("Clear")
